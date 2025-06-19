0129fe2f5a87402a408502550f0de219d2807bbc
not-learning <kaa.smart@gmail.com>
2025-06-20 04:53:11 +0500

    Fixed button name

--- web/einmaleins.service.worker.js
+++ web/einmaleins.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1750336719|6005107459';
+const CACHE_VERSION = '1750377064|58902242';
 /** @type {string} */
 const CACHE_PREFIX = 'Einmaleins-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
